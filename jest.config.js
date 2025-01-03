9f6a018bfd685ce9d042df3d0638b2ab269d2e6a
krebsni <nicokrebs97@gmail.com>
2025-01-03 17:38:52 +0100

    initial commit, sample problem

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+// filepath: /typescript-playground/jest.config.js
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/tests/**/*.test.ts'],
+};
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { twoSum } from './problems/twoSum';
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+console.log(twoSum(nums, target));
